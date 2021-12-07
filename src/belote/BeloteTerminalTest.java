ffd70af0f8dd8cef47e9e87794a2ba25eb63c2a6
barseghatanyan <barsegh.atanyan@tumo.org>
2021-12-07 20:31:37 +0400

    fix contract bug

--- src/belote/BeloteTerminalTest.java
+++ src/belote/BeloteTerminalTest.java
@@ -20,12 +20,14 @@ public class BeloteTerminalTest extends TerminalTest {
         BeloteState beloteState = (BeloteState) state;
         boolean isTerminal = beloteState.getApplicableActions().isEmpty();
         if(isTerminal){
+            int threshold = 20 + NUMBER_OF_SUITS * 15;
+            boolean madeContract =  beloteState.getScore() >= threshold;
             if(winLose){
-                int threshold = 20 + NUMBER_OF_SUITS * 15;
-                int result = beloteState.getScore() >= threshold ? 1 : 0;
+                int result = madeContract ? 1 : 0;
                 utilities.put(state, result);
             } else {
-                utilities.put(state, beloteState.getScore());
+                int result = madeContract ? beloteState.getScore() : 0;
+                utilities.put(state, result);
             }
         }
 
